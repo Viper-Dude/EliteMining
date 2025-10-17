30a2d51ebb146008c766aaa2857d72f84f49323a
ViperDude <mrd607@gmail.com>
2025-10-17 17:30:15 +0200

    v4.3.1 - Add Coltan material support and update Discord link

--- app/main.py
+++ app/main.py
@@ -365,7 +365,7 @@ class TextOverlay:
             self.overlay_window = None
 
 APP_TITLE = "EliteMining"
-APP_VERSION = "v4.3.0"
+APP_VERSION = "v4.3.1"
 PRESET_INDENT = "   "  # spaces used to indent preset names
 
 LOG_FILE = os.path.join(os.path.expanduser("~"), "EliteMining.log")
